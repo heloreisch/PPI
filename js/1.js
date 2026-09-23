fc0bd62e015586f1301cdda6aadf1b6bcb197dac
heloreisch <heloisa.mafrat@gmail.com>
2026-09-23 09:00:53 -0300

    javas

---
+++ js/1.js
@@ -0,0 +1,24 @@
+
+/*tipos de dado em j
+undefined
+Number
+String
+Object
+null
+var i=0; -> aponta para "caixa e emora" que guarda numbers 
+i = s -> sring p nuber -> ele destroi a variavvel de cima que cria em sring e quadra na "caixastring"
+*/
+
+console.log("olá");
+// declara variavis em js
+i=0; // variavel global var
+var nome="Heloisa";// define variave global
+let numero; //deine variavel local por bloco
+const numeroPI = 3.1415
+
+
+console.log("Variavel i:" +typeof(i));
+//Variavel i:number (vai mostrar number porque i temmum numro inteiro pos )
+console.log("Variavel i:" +typeof(nome));
+// Variavel i:undefined (não deiniu o nome var nome;)
+// Variavel i:string(string pq temum nome)
